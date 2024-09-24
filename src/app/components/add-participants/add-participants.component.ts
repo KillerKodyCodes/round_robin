782f1d14f4ab6d1a9653ed524d79c375ef972341
killerkodycodes <killerbyrd12@gmail.com>
2024-09-23 22:05:46 -0500

    added initial components and routes to those components

---
+++ src/app/components/add-participants/add-participants.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-participants',
+  standalone: true,
+  imports: [],
+  templateUrl: './add-participants.component.html',
+  styleUrl: './add-participants.component.scss'
+})
+export class AddParticipantsComponent {
+
+}
